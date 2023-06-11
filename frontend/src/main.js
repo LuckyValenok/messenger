5b7809e4de5f150b3f08b8399d1b56af3339f9f9
evabronskayaa <eva27122002b@gmail.com>
2023-06-11 21:19:34 +0500

    Добавлена страница авторизации

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import './assets/styles/main.css'
+import {createRouter, createWebHistory} from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+import SignUp from "@/components/SignUp/SignUp.vue";
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'root',
+            component: HelloWorld
+        },
+        {
+            path: '/signup',
+            name: 'signup',
+            component: SignUp
+        }
+    ]
+})
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
