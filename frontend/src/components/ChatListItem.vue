c19504b0e8686b7dcc0ffcdddcc369630c8bfcf3
evabronskayaa <eva27122002b@gmail.com>
2023-06-14 00:11:40 +0500

    Чаты из списка теперь отображают свое содержимое корректно

--- frontend/src/components/ChatListItem.vue
+++ frontend/src/components/ChatListItem.vue
@@ -2,15 +2,17 @@
   <div class="chat-item" :class="{active: chat.id === selectChatId}">
     <a @click="select">
       <div class="chat-item-content">
-        <div>
-          <img :src="getImgUrl(chat.type)" alt="avatar">
-        </div>
-        <div class="chat-item-content--text">
-          <p style="color: #411467;">{{ chat.name }}</p>
-          <p v-if="chat.message">{{ chat.message.text }}</p>
+        <div class="flex-row">
+          <div>
+            <img :src="getImgUrl(chat.type)" alt="avatar">
+          </div>
+          <div class="chat-item-content--text">
+            <p style="color: #411467;">{{ chat.name }}</p>
+            <p v-if="chat.message">{{ chat.message.text }}</p>
+          </div>
         </div>
         <div v-if="chat.message">
-          <p style="width: 70px;">{{ time }}</p>
+          <p style="width: 120px; text-align: right">{{ time }}</p>
         </div>
       </div>
     </a>
